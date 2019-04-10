dc1b0e87bbe39f29f7d5b5ad64c375518f38ff14
Matt Perkins <hello@mattperkins.me>
2019-04-10 12:57:23 -0400

    update build command

--- deploy.js
+++ deploy.js
@@ -13,7 +13,7 @@ var config = {
   localRoot: __dirname + '/dist',
   remoteRoot: secrets.ftpremotedir,
   include: ['*', '**/*'],      // this would upload everything except dot files
-  exclude: ['**/*.map'],     // e.g. exclude sourcemaps - ** exclude: [] if nothing to exclude **
+  // exclude: ['**/*.map'],     // e.g. exclude sourcemaps - ** exclude: [] if nothing to exclude **
   deleteRemote: true,              // delete existing files at destination before uploading
   forcePasv: true                 // Passive mode is forced (EPSV command is not sent)
 };
